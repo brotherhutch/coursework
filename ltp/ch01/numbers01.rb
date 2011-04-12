f160436fb2b1866c79759fee3c1f8740d7b757ed
Andy Lindeman <andy@highgroove.com>
2011-04-11 20:22:11 -0500

    Add basis for Ch01 coursework

---
+++ ltp/ch01/numbers01.rb
@@ -0,0 +1,15 @@
+# Replace "nil" with math to calculate the number of hours in a year
+hours_in_year = nil
+puts "There are #{hours_in_year} hours in a year"
+
+# Replace "nil" with math to calculate the number of minutes in a decade
+minutes_in_decade = nil
+puts "There are #{minutes_in_decade} minutes in a decade"
+
+# Replace "nil"
+seconds_old = nil
+puts "I am #{seconds_old} seconds old"
+
+# Replace "nil"
+chocolates_in_lifetime = nil
+puts "I hope to eat #{chocolates_in_lifetime} chocolates in my lifetime"
---
+++ ltp/ch01/numbers02.rb
@@ -0,0 +1,5 @@
+seconds_old = 1031 * 1000000
+
+# Replace "nil" with math to determine the number of years in seconds_old
+years_old = nil
+puts "Given I am #{seconds_old} seconds old, I am #{years_old} years old"
