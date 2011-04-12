f35d855ebb565e90235e459d7b141ad0655eb7be
Andy Lindeman <andy@highgroove.com>
2011-04-11 20:25:34 -0500

    Add basis for Ch04 coursework

---
+++ ltp/ch04/favorite_number.rb
@@ -0,0 +1,3 @@
+# Write a program which asks for a person's favorite number. Have your program
+# add one to the number, then suggest the result as a bigger and better
+# favorite number. (Do be tactful about it, though.)
---
+++ ltp/ch04/greeting.rb
@@ -0,0 +1,2 @@
+# Write a program which asks for a person's first name, then middle, then last.
+# Finally, it should greet the person using their full name.
