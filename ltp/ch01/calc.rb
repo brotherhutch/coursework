e8739486c9150c3d0170058890d419c8e919ae6a
Mark Hutchison <mark.d.hutchison@gmail.com>
2011-04-23 12:37:33 -0700

    Week 1 exercises complete in two new files, calc and strings

---
+++ ltp/ch01/calc.rb
@@ -0,0 +1,10 @@
+﻿puts 'Hours in a year'
+puts 365 * 24
+puts 'Minutes in a decade'
+puts 10 * 365 * 24 * 60
+puts 'Seconds old (aprox)'
+puts 34 * 365 * 24 * 60 * 60
+puts 'How many chocolates in lifetime'
+puts 5 * 52 * 50
+puts 'Age in years at 1031 million seconds old'
+puts 1031000000 / 60 / 60 / 24 / 365
---
+++ ltp/ch04/strings.rb
@@ -0,0 +1,16 @@
+﻿puts 'Write a program which asks for a person\'s first name, then middle, then last. Finally, it should greet the person using their full name.'
+puts ''
+puts 'First name:'
+fn = gets.chomp
+puts 'Middle name:'
+mn = gets.chomp
+puts 'Last name:'
+ln = gets.chomp
+puts 'Hello, ' + fn + ' ' + mn + ' ' + ln + '.'
+puts ''
+puts 'Write a program which asks for a person\'s favorite number. Have your program add one to the number, then suggest the result as a bigger and better favorite number. (Do be tactful about it, though.)'
+puts ''
+puts 'What is your favourite number ' + fn + '?'
+fav = gets.chomp.to_i
+fav = fav + 1
+puts 'My favourite number is ' + fav.to_s + ', which is clearly a better number than yours.'
